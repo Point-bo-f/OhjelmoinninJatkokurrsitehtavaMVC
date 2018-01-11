dad7c16940b091c9a1a28aa505725da8f5709ffe
Bo Furustam <bo.furustam@stud.pointcollege.fi>
2018-01-11 16:27:18 +0200

    Versio 2

--- OhjelmoinninJatkokurssiMVC/OhjelmoinninJatkokurssiMVC/Controllers/ProjektitController.cs
+++ OhjelmoinninJatkokurssiMVC/OhjelmoinninJatkokurssiMVC/Controllers/ProjektitController.cs
@@ -12,28 +12,16 @@ namespace OhjelmoinninJatkokurssiMVC.Controllers
 {
     public class ProjektitController : Controller
     {
-        AsiakastietokantaEntities db = new AsiakastietokantaEntities();
-
-        // GET: Henkilots
+        // GET: Customer
         public ActionResult Index()
         {
-            {
-                List<Projektit> model = new List<Projektit>();
-                try
-                {
-                    AsiakastietokantaEntities entities = new AsiakastietokantaEntities();
-                    model = entities.Projektit.ToList();
+            ViewBag.OmaTieto = "ABC123";
 
-                    entities.Dispose();
-                }
+            AsiakastietokantaEntities entities = new AsiakastietokantaEntities();
+            List<Projektit> model = entities.Projektit.ToList();
+            entities.Dispose();
 
-                catch (Exception ex)
-                {
-                    ViewBag.ErrorMessage = ex.GetType() + ": " + ex.Message;
-                }
-
-                return View(model);
-            }
+            return View(model);
         }
 
         public ActionResult Index2()
@@ -41,139 +29,21 @@ namespace OhjelmoinninJatkokurssiMVC.Controllers
             return View();
         }
 
-        // GET: Henkilots/Details/5
-        public ActionResult Details(int? id)
+        public ActionResult Index3()
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Projektit projektit = db.Projektit.Find(id);
-            if (projektit == null)
-            {
-                return HttpNotFound();
-            }
-            return View(projektit);
-        }
-
-        // GET: Henkilots/Create
-        public ActionResult Create()
-        {
-            AsiakastietokantaEntities db = new AsiakastietokantaEntities();
-
-            Projektit model = new Projektit();
-
-            return View(model);
-
-
-        }
-
-        // POST: Henkilots/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(Projektit model)
-        {
-            AsiakastietokantaEntities db = new AsiakastietokantaEntities();
-
-            Projektit projektit = new Projektit();
-            projektit.ProjektiID = model.ProjektiID;
-            projektit.Projektinimi = model.Projektinimi;
-           
-            db.Projektit.Add(projektit);
-
-            try
-            {
-                db.SaveChanges();
-            }
-
-            catch (Exception ex)
-            {
-            }
-
-
-            return RedirectToAction("Index");
-        }
-
-
-
-
-        // GET: Henkilots/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Projektit projektit = db.Projektit.Find(id);
-            if (projektit == null)
-            {
-                return HttpNotFound();
-            }
-            return View(projektit);
-        }
-
-        // POST: Henkilots/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ProjektiID,Projektinimi")] Projektit projektit)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(projektit).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(projektit);
-        }
-
-        // GET: Henkilots/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-           Projektit projektit = db.Projektit.Find(id);
-            if (projektit == null)
-            {
-                return HttpNotFound();
-            }
-            return View(projektit);
-        }
-
-        // POST: Henkilots/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Projektit projektit = db.Projektit.Find(id);
-            db.Projektit.Remove(projektit);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
+            return View();
         }
 
         public JsonResult GetList()
         {
             AsiakastietokantaEntities entities = new AsiakastietokantaEntities();
+            //List<Customer> model = entities.Customers.ToList();
 
             var model = (from p in entities.Projektit
                          select new
                          {
                              ProjektiID = p.ProjektiID,
-                             Projektinimi = p.Projektinimi,
+                             Projektinimi = p.Projektinimi,                       
                          }).ToList();
 
             string json = JsonConvert.SerializeObject(model);
@@ -184,16 +54,16 @@ namespace OhjelmoinninJatkokurssiMVC.Controllers
 
             return Json(json, JsonRequestBehavior.AllowGet);
         }
+
         public JsonResult GetSingleProjekti(int id)
         {
             AsiakastietokantaEntities entities = new AsiakastietokantaEntities();
-
             var model = (from p in entities.Projektit
                          where p.ProjektiID == id
                          select new
                          {
                              ProjektiID = p.ProjektiID,
-                             Projektinimi = p.Projektinimi,
+                             Projektinimi = p.Projektinimi,                            
                          }).FirstOrDefault();
 
             string json = JsonConvert.SerializeObject(model);
@@ -202,36 +72,38 @@ namespace OhjelmoinninJatkokurssiMVC.Controllers
             return Json(json, JsonRequestBehavior.AllowGet);
         }
 
-        public ActionResult Edit2(Projektit proj)
+        public ActionResult Update(Projektit proj)
         {
             AsiakastietokantaEntities entities = new AsiakastietokantaEntities();
+            string id = proj.ProjektiID.ToString();
 
             bool OK = false;
 
-            if (proj.ProjektiID == 0)
-            {
+            // onko kyseessä muokkaus vai uuden lisääminen?
+            if (id == "uusi")            {
+                // kyseessä on uuden asiakkaan lisääminen, kopioidaan kentät
                 Projektit dbItem = new Projektit()
                 {
-                    ProjektiID = proj.ProjektiID,
-                    Projektinimi = proj.Projektinimi,
+                //    ProjektiID = proj.Projektinimi.ToString(),                    
                 };
 
+                // tallennus tietokantaan
                 entities.Projektit.Add(dbItem);
                 entities.SaveChanges();
                 OK = true;
             }
             else
             {
-                Projektit dbItem = (from p in entities.Projektit
-                                   where p.ProjektiID == proj.ProjektiID
-                                   select p).FirstOrDefault();
-
-
-                if (dbItem != null)
+                // muokkaus, haetaan id:n perusteella riviä tietokannasta
+                //Projektit dbItem = (from p in entities.Projektit
+                //                   where p.ProjektiID == id
+                //                   select p).FirstOrDefault();
+                //if (dbItem != null)
                 {
-                    dbItem.ProjektiID = proj.ProjektiID;
-                    dbItem.Projektinimi = proj.Projektinimi;
-                    
+                    //dbItem.Projektinimi = proj.Projektinimi;
+                   
+
+                    // tallennus tietokantaan
                     entities.SaveChanges();
                     OK = true;
                 }
@@ -240,16 +112,19 @@ namespace OhjelmoinninJatkokurssiMVC.Controllers
 
             return Json(OK, JsonRequestBehavior.AllowGet);
         }
-        public ActionResult Delete(int id)
+
+        public ActionResult Delete(string id)
         {
             AsiakastietokantaEntities entities = new AsiakastietokantaEntities();
 
+            // etsitään id:n perusteella asiakasrivi kannasta
             bool OK = false;
             Projektit dbItem = (from p in entities.Projektit
-                               where p.ProjektiID == id
+                                where p.ProjektiID.ToString() == id
                                select p).FirstOrDefault();
             if (dbItem != null)
             {
+                // tietokannasta poisto
                 entities.Projektit.Remove(dbItem);
                 entities.SaveChanges();
                 OK = true;
